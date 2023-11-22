e818a34b12148ea5a908f22d4ecb5173a06cb674
iAMPeacemaker <damir_mu97@bk.ru>
2023-11-22 19:38:31 +0600

    Add Sem2

---
+++ seminar2/seminar2_task1.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE gb_seminar2;
+
+USE gb_seminar2;
+
+CREATE TABLE sales (
+    id INT,
+    order_date Date NOT NULL,
+    bucket VARCHAR(50)
+    );
+
+INSERT INTO sales (id, order_date, bucket) VALUES (
+    1, "2021-01-01", "101 to 300"
+    );
+
+INSERT INTO sales (id, order_date, bucket) VALUES (
+    1, "2021-01-02", "101 to 300"
+    );
+
+INSERT INTO sales (id, order_date, bucket) VALUES (
+    1, "2021-01-03", "less than equal to 100"
+    );
+
+INSERT INTO sales (id, order_date, bucket) VALUES (
+    1, "2021-01-04", "101 to 300"
+    );
+
+INSERT INTO sales (id, order_date, bucket) VALUES (
+    1, "2021-01-05", "greater than 300"
+    );
